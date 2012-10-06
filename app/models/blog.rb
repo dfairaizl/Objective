8c776844c40f00c45b61a9b1f2294e8e702e6e23
Dan Fairaizl <dan@basicallybits.com>
2012-10-06 12:10:59 -0600

    Initial blog commit and basic design

---
+++ app/models/blog.rb
@@ -0,0 +1,5 @@
+class Blog
+  include MongoMapper::Document
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Objective::Application.config.secret_token = 'c43482f1bbd63c00a9e618ec5af3480f9de73588e287a1ca9a5ebf8684575dda3e4d060ea22ff8a01d519267fe8f11291376a34db9e9db2fa438b73a782e7e94'
