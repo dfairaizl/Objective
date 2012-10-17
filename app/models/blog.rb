c35710f414ce8aed5e13df127854e71f212d5284
Dan Fairaizl <dan@basicallybits.com>
2012-10-17 15:17:00 -0600

    Basic blog model

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,15 @@
 class Blog
 
-
+	include Mongoid::Document
+	
+	#DB Fields
+	field :blog_name, type: String
+	field :blog_description, type: String
+	
+	#Validation
+	validates :blog_name, presence: true
+	
+	#Associations
+	has_many :posts, validate: false
 
 end
