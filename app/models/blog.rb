aa03eccc91475a8afea5c731c974b388356ced8b
Dan Fairaizl <dan@basicallybits.com>
2012-10-17 15:06:28 -0600

    Removed mongomapper

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,12 +1,5 @@
 class Blog
-  include MongoMapper::Document
 
-  #keys
-  key :blog_name, String
-  
-  #associations
-  has_many :posts
-  
-  timestamps!
+
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,3 @@
 class Post
-  include MongoMapper::Document
-  
-  key :title, String
-  key :status, Integer
-  key :body, String
-  
-  belongs_to :blog
-  
-  timestamps!
 
 end
