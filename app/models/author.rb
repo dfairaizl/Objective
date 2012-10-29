6c2968c9e89a99fed41c9cbca8cf285ccadf9a00
Dan Fairaizl <dan@basicallybits.com>
2012-10-28 20:29:18 -0600

    Cleaned up Author model devise methods

--- app/models/author.rb
+++ app/models/author.rb
@@ -5,8 +5,7 @@ class Author
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+   devise :database_authenticatable, :registerable, :rememberable, :validatable
 
   ## Database authenticatable
   field :email,              :type => String, :default => ""
